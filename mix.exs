b88d5e5bf33b7fac8192c10f9003b90c86454815
Chris Duranti <chrisd1891@gmail.com>
2018-09-04 13:48:06 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Exkml.Mixfile do
   def project do
     [
       app: :exkml,
-      version: "0.2.6",
-      elixir: "~> 1.5",
+      version: "0.3.0",
+      elixir: "~> 1.7",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       description: description(),
