c0cd56db39de0fb97a3afa8c3a20c75bf71c9e44
Bodo Tasche <bodo@wannawork.de>
2021-06-21 11:54:03 +0200

    update gen_stage

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Exkml.Mixfile do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:gen_stage, "~> 0.14"},
+      {:gen_stage, "~> 1.1.0"},
       {:saxy, "~> 0.9.1"}
     ]
   end
